fb3bd24fa9d25a15c1fac89692738f47a9c3a9c0
RonZatkovsky <167799853+RonZatkovsky@users.noreply.github.com>
2024-08-22 01:26:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let suits = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
   //write your code here
+  randomCard();
+
+  document.querySelector("#card").addEventListener("click", randomCard);
   console.log("Hello Rigo from the console!");
 };
+
+function randomCard() {
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+
+  let cardColor = suit == "♦" || suit == "♥" ? "red" : "black";
+
+  let components;
+  let top = document.querySelector("#top");
+  top.style.color = cardColor;
+  top.innerHTML = suit;
+  let setNumber = document.querySelector("#number");
+  setNumber.style.color = cardColor;
+  setNumber.innerHTML = number;
+  let bottom = document.querySelector("#bottom");
+  bottom.style.color = cardColor;
+  bottom.innerHTML = suit;
+}
